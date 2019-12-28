f7b3742375138dd2c85ae1d1e7812b479914c300
tomoyk <tmyk.kym@gmail.com>
2019-12-28 12:40:29 +0900

    del DS_Store and gitignore

--- conf-server/app.py
+++ conf-server/app.py
@@ -4,29 +4,23 @@ import yaml
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False  # JSONでの日本語文字化け対策
 
-'''
-READ: インストール状況を取得
-'''
 @app.route('/status', methods=['GET'])
 def post_json():
+    """ READ: インストール状況を取得 """
     req_json = request.get_json()
     return jsonify(req_json)
 
 
-'''
-UPDATE: インストールの状況を追加/更新
-'''
 @app.route('/status', methods=['POST'])
 def update_status():
+    """ UPDATE: インストールの状況を追加/更新 """
     req_json = request.get_json()
     return jsonify(req_json)
 
 
-'''
-READ: インストール設定を取得
-'''
 @app.route('/settings', methods=['GET'])
 def read_setting():
+   """ READ: インストール設定を取得 """
     with open('config.yaml') as f:
         file_content = yaml.load(f, Loader=yaml.SafeLoader)
     return jsonify(file_content)
