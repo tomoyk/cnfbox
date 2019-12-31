e69eed755c41f0175a226ac6a34ba2597c86818c
tomoyk <tmyk.kym@gmail.com>
2019-12-31 22:09:03 +0900

    fix cli-option

--- cnfbox-cli/cnfbox.py
+++ cnfbox-cli/cnfbox.py
@@ -1,6 +1,17 @@
 from item import os, user, network
 
 
+def _get_json(dat: object) -> object:
+    """ ObjectをJsonへ変換 """
+    def default_method(item):
+        if isinstance(item, object) and hasattr(item, '__dict__'):
+            return item.__dict__
+        else:
+            raise TypeError
+    import json
+    return json.dumps(dat, default=default_method, indent=4)
+
+
 class CnfBox(object):
     def __init__(self):
         self.os = os.Os()
@@ -12,14 +23,31 @@ class CnfBox(object):
             print(f'{k}:'.ljust(10) + f'{v}')
 
     def show_json(self):
-        # ObjectをJsonへ変換
-        def default_method(item):
-            if isinstance(item, object) and hasattr(item, '__dict__'):
-                return item.__dict__
-            else:
-                raise TypeError
-        import json
-        print(json.dumps(self.__dict__, default=default_method, indent=4))
+        print(_get_json(self.__dict__))
 
-    def commit(self, parameter_list):
-        pass
+    def commit(self):
+        """ Get REMOTE URL """
+        import os
+        REMOTE_URL = os.getenv('REMOTE_URL')
+        assert REMOTE_URL is None, "Env Var 'REMOTE_URL' is not set."
+
+        """ Build HTTP Request """
+        from urllib import request
+        import json
+        payload = _get_json(self.__dict__)
+        req_body = json.dumps(payload).encode('utf-8')
+        req_method = 'POST'
+        req_headers = {
+            'Content-Type': 'application/json',
+        }
+        req = request.Request(
+            REMOTE_URL,
+            data=req_body,
+            method=req_method,
+            headers=req_headers)
+        try:
+            with request.urlopen(req) as res:
+                res_body = res.read().decode('utf-8')
+                print(res_body)
+        except Exception as e:
+            print(e)
--- cnfbox-cli/item/network.py
+++ cnfbox-cli/item/network.py
@@ -28,8 +28,7 @@ class Network(item.base.Base):
             ipaddr: str = '192.168.0.2',
             cidr: int = 24,
             gateway: str = '192.168.0.1',
-            dns: str = '1.1.1.1',
-    ):
+            dns: str = '1.1.1.1',):
         self.device = device
         self.iptype = _validate_iptype_format(iptype)
         self.ipaddr = _validate_ip_format(ipaddr)
