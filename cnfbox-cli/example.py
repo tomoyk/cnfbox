d3ede7ac565931a83e99bdda55c1a4c1800f4814
tomoyk <tmyk.kym@gmail.com>
2020-01-04 18:18:07 +0900

    fix example

--- cnfbox-cli/example.py
+++ cnfbox-cli/example.py
@@ -1,7 +1,18 @@
 from cnfbox import CnfBox
+
 cb = CnfBox()
+
 cb.os.name = 'centos'
 cb.os.version = 8.0
 cb.os.lang = 'us'
+
 cb.user = ['alice', 'bob']
+
+cb.network.device = 'ens160'
+cb.network.iptype = 'static'
+cb.network.ipaddr = '10.200.1.5'
+cb.network.cidr = 16
+cb.network.gateway = '10.200.1.254'
+cb.network.dns = '10.200.1.3'
+
 print(cb.show_json())
