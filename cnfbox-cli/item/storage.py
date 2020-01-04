c84598af9203d849a8035294bbed4676d9a08a5a
tomoyk <tmyk.kym@gmail.com>
2020-01-04 23:50:03 +0900

    add storage

--- cnfbox-cli/item/storage.py
+++ cnfbox-cli/item/storage.py
@@ -2,4 +2,17 @@ import item.base
 
 
 class Storage(item.base.Base):
-    pass
+    __BOOTLOADER_LIST: tuple = ('none', 'mbr', 'uefi', 'kernel')
+
+    def __init__(
+            self,
+            bootloader: str,
+            auto_partition: bool,
+            clear_partition: bool):
+        assert bootloader in self.__BOOTLOADER_LIST, "Invalid Bootloader Location given."
+        self.bootloader = bootloader
+        self.auto_partition = auto_partition
+        self.clear_partition = clear_partition
+
+    def __repr__(self):
+        return super().__repr__()
