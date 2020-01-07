0b6a892f9d2da5586923d2b90866906b84c9e285
tomoyk <tmyk.kym@gmail.com>
2020-01-07 14:20:29 +0900

    add sql-support

--- cnfbox-srv/main.py
+++ cnfbox-srv/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False  # JSONでの日本語文字化け対策
@@ -9,19 +12,26 @@ def index():
     return 'It works'
 
 
-@app.route('/conf/<int:id>', methods=['GET'])
-def post_json(id):
-    return f'todo: get conf {id}'
-
-
-@app.route('/conf', methods=['GET'])
-def create_conf():
-    return 'There is nothing'
+@app.route('/conf/<int:conf_id>', methods=['GET'])
+def read_conf(conf_id):
+    # 読み取り
+    # 200 Fetch
+    return f'todo: get conf {conf_id}'
 
 
 @app.route('/conf', methods=['POST'])
+def create_conf():
+    req_json = request.get_json()
+    # 書き込み
+    # 201 Created
+    return jsonify(req_json)
+
+
+@app.route('/conf/<int:id>', methods=['PUT'])
 def update_status():
     req_json = request.get_json()
+    # 書き込み
+    # 200 Update
     return jsonify(req_json)
 
 
